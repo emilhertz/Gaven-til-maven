1f92fabcd7182b66a9effadad14a48e13a16d255
pvburleigh <pvburleigh@gmail.com>
2020-02-11 09:39:28 +0100

    Address class

---
+++ javaScript/Classes/Address.js
@@ -0,0 +1,10 @@
+class Address {
+    constructor(id, streetName, streetNumber, zipCode, city, country){
+        this.id = id;
+        this.streetName = streetName;
+        this.streetName = streetNumber;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.country = country;
+    }
+}
