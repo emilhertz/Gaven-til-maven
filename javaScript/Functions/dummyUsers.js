daed1d317c2f56fe76d62d1aba51bd1f280614b0
emilhertz <emhe19ae@student.cbs.dk>
2020-02-11 10:49:26 +0100

    login-funktion

--- javaScript/Functions/dummyUsers.js
+++ javaScript/Functions/dummyUsers.js
@@ -11,3 +11,22 @@ function dummyUsers() {
 };
 //Calling dummyUser() to save dummy users in localStorage
 dummyUsers();
+
+//login metode
+function login() {
+    let username = document.getElementById("loginUsername");
+    let password = document.getElementById("loginPassword");
+
+    //for-loop der bruger operators til at se om indtastede værdier stemmer overens med listOfUsers-array, som er gemt i localStorage
+    for (let i=0; i < listOfUsers.length; i++) {
+        if (username.value === listOfUsers[i].username && password.value === listOfUsers[i].password) {
+            //Gemmer bruger som logger ind i localStorage og som string
+            let signedIn = JSON.stringify(listOfUsers[i]);
+            localStorage.setItem("signedIn", signedIn);
+            // Åbner home.html
+            window.open("../HTML/home.html", "_self");
+            return
+        }
+    }
+    alert("Forkert brugernavn eller password :(")
+};
