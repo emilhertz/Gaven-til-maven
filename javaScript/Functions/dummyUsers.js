a21b489f033f5535cc440d26bc141e1c6fc8383e
pvburleigh <pvburleigh@gmail.com>
2020-02-11 10:37:18 +0100

    dummy users saved in local storage by default

--- javaScript/Functions/dummyUsers.js
+++ javaScript/Functions/dummyUsers.js
@@ -1,11 +1,13 @@
 function dummyUsers() {
     if (localStorage.getItem("storedListOfUsers") == null) {
         let listOfUsers = [];
-        listOfUsers.push(new Users("Thorn","password","32","København"));
-        listOfUsers.push(new Users("Peter","Kanin","224","Eventyrskoven"));
+        listOfUsers.push(new User(1,"Peter","Kanin","Rabbithole", "1234", "playboy@gmail.com"));
+        listOfUsers.push(new User(2,"Emil","Trækkerdreng","Grindrboi", "<=3", "hola@gmail.com"));
 
         //listOfUsers stringifies, så de kan tilknyttes localStorage
         let listOfUsersString = JSON.stringify(listOfUsers);
         localStorage.setItem("storedListOfUsers", listOfUsersString);
     }
 };
+//Calling dummyUser() to save dummy users in localStorage
+dummyUsers();
