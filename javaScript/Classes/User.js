04fd28f1fddfc218eaf7259d262b40fede4e2e72
pvburleigh <pvburleigh@gmail.com>
2020-02-11 09:25:45 +0100

    classes

---
+++ javaScript/Classes/User.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(id, firstName, lastName, userName, password, email) {
+        this.id = id;
+        this.firstname = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    login(){
+
+    }
+
+    logout(){
+
+    }
+
+    newReservation(){
+
+    }
+    
+}
