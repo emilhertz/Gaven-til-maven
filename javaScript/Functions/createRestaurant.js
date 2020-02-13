7223f34e6e9ebf2716e64f4d166e90b849bf5598
emilhertz <emhe19ae@student.cbs.dk>
2020-02-13 15:49:32 +0100

    createRestaurant.js

---
+++ javaScript/Functions/createRestaurant.js
@@ -0,0 +1,16 @@
+function createRestaurant(){
+    //Firstly we assign "createRestaurants" and "form" to variables
+    let restaurantDiv = document.getElementById("createRestaurant");
+    let createRestaurantForm = document.createElement("form");
+
+    //Here we create a new input and manipulate the DOM-structure.
+    // Afterwards we assign type, placeholder and id to the input.
+    let restaurantName = document.createElement("input");
+    restaurantName.type = "text";
+    restaurantName.placeholder = "Restaurant Navn";
+    restaurantName.id = "restaurantId";
+
+    //We call the function
+    createRestaurantForm.appendChild(restaurantName);
+    restaurantDiv.appendChild(createRestaurantForm);
+}
