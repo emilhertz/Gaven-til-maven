7f0608bce80c1050d49666f043e637a2df3315d9
kristofferkristensen <krkr19ad@student.cbs.dk>
2020-02-18 22:11:58 +0100

    Seat dropdown er lavet

--- javaScript/Functions/createRestaurant.js
+++ javaScript/Functions/createRestaurant.js
@@ -4,6 +4,8 @@ function createRestaurant() {
     let restaurantDiv = document.getElementById("createRestaurant");
     let form = document.createElement("form");
     let br = document.createElement("br");
+    let restaurantParagraph1 = document.createElement("p");
+    let restaurantTextNode = "Restaurant: ";
 
     //Here we create a new input and manipulate the DOM-structure.
     // Afterwards we assign type, placeholder and id to the input.
@@ -13,6 +15,8 @@ function createRestaurant() {
     restaurantName.id = "restaurantId";
 
     //We call the function
+    restaurantParagraph1.appendChild(document.createTextNode(restaurantTextNode));
+    form.appendChild(restaurantParagraph1);
     form.appendChild(restaurantName);
     form.appendChild(br);
 
@@ -25,6 +29,8 @@ function createRestaurant() {
     let seatNumber = document.createElement("option");
     let seatNumber2 = document.createElement("option");
     let seatNumber3 = document.createElement("option");
+    let pladser = document.createElement("p");
+    let pladsTextNode = "Antal gæster: ";
 
     createSeatDropdown.type = "text";
     createSeatDropdown.id = "myDropdownList";
@@ -37,6 +43,8 @@ function createRestaurant() {
     createSeatDropdown.add(seatNumber2);
     createSeatDropdown.add(seatNumber3);
 
+    pladser.appendChild(document.createTextNode(pladsTextNode));
+    form.appendChild(pladser);
     form.appendChild(createSeatDropdown);
     //Uses the "cloneNode()" to clone the break line.
     form.appendChild(br.cloneNode());
@@ -44,22 +52,31 @@ function createRestaurant() {
 
     //Reservations
     let reservationName = document.createElement("input");
+    let maxGuests = document.createElement("p");
+    let maxGuestsText = "Max antal gæster: ";
 
     reservationName.type = "number";
     reservationName.placeholder = "Type the max seats here..";
     reservationName.id = "reservationId";
 
+    maxGuests.appendChild(document.createTextNode(maxGuestsText));
+    form.appendChild(maxGuests);
     form.appendChild(reservationName);
     //Uses the "cloneNode()" to clone the break line.
     form.appendChild(br.cloneNode());
 
     //Description
     let descriptionName = document.createElement("input");
+    let description = document.createElement("p");
+    let descriptionText = "Kort beskrivelse af restauranten";
 
     descriptionName.type = "text";
     descriptionName.placeholder = "Placér en beskrivelse her";
     descriptionName.id = "descriptionId";
 
+
+    description.appendChild(document.createTextNode(descriptionText));
+    form.appendChild(description);
     form.appendChild(descriptionName);
 
     restaurantDiv.appendChild(form);
