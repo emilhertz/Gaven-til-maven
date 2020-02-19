49e847af8459504c031fd91039d8d70c675366cc
emilhertz <emhe19ae@student.cbs.dk>
2020-02-19 16:18:54 +0100

    Navbar update navbar.ejs

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 //imports modules
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 //new app
 const app = new express();
+app.set('view engine', 'ejs');
 
 //use public directory for static files
-app.use(express.static('public'));
+//skal måske slettes
+app.use(express.static(path.resolve(__dirname,'public')));
 
 //assigns port
 app.listen(420, ()=>{console.log('App listening on port 420')});
 
-//defining paths
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+//assigning paths
+app.get('/', (req, res)=> {
+    res.render('index')
+});
+
+app.get('/adminPage', (req, res)=> {
+    res.render('adminPage')
+});
+
+app.get('/login', (req, res)=> {
+    res.render('login')
+});
+
+app.get('/reservations', (req, res)=> {
+    res.render('reservations')
+});
+
+app.get('/reserve', (req, res)=> {
+    res.render('reserve')
 });
