4a344c5c9a01b771dcb0bb4dd6212bce1efb703a
emilhertz <emhe19ae@student.cbs.dk>
2020-02-19 15:24:23 +0100

    static error

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 //imports modules
 const express = require('express');
+const path = require('path');
 
 //new app
 const app = new express();
 
+//use public directory for static files
+app.use(express.static('public'));
+
 //assigns port
 app.listen(420, ()=>{console.log('App listening on port 420')});
 
+//defining paths
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+});
